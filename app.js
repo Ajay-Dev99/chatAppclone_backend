3c7e33c06d6243c8951516bff2373113cfa0daf9
ajaydev <ajaydevkomath@gmail.com>
2025-12-04 22:53:31 +0530

    Update CORS configuration to allow requests from the specified production URL while maintaining local development support.

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ connectDB();
 
 app.use(
     cors({
-        origin: process.env.CLIENT_URL || "http://localhost:5173",
+        origin: ["https://whatsapp-clone-silk-rho.vercel.app", process.env.CLIENT_URL || "http://localhost:5173"],
         credentials: true,
         methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
     })
